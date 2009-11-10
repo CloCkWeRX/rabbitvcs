CloCkWeRX/rabbitvcs

commit 47089783087d8caad2d25dce2769c415989f3eed

Don't show the context menu by default.  Separate the show action into a new method so that the menu items can possibly be manipulated before showing.
git-svn-id: http://rabbitvcs.googlecode.com/svn/trunk@2005 a8b49908-ab93-11de-b6f4-e1be98fab775

rabbitvcs/lib/contextmenu.py
@@ -590,7 +590,7 @@ class GtkFilesContextMenu:
                 added_ignore_labels.append(ext_str)
 
         # Generate the full context menu
-        context_menu = GtkContextMenu([
+        self.items = [
             {
                 "label": _("View Diff"),
                 "icon": "rabbitvcs-diff",
@@ -734,5 +734,8 @@ class GtkFilesContextMenu:
                     "args": None
                 }
             }
-        ])
+        ]
+    
+    def show(self):
+        context_menu = GtkContextMenu(self.items)
         context_menu.show(self.event)

rabbitvcs/ui/add.py
@@ -178,7 +178,7 @@ class Add(InterfaceView, GtkContextMenuCaller):
 
     def show_files_table_popup_menu(self, treeview, data):
         paths = self.files_table.get_selected_row_items(1)
-        GtkFilesContextMenu(self, data, self.base_dir, paths)
+        GtkFilesContextMenu(self, data, self.base_dir, paths).show()
 
 if __name__ == "__main__":
     from rabbitvcs.ui import main

rabbitvcs/ui/checkmods.py
@@ -148,7 +148,7 @@ class CheckForModifications(InterfaceView, GtkContextMenuCaller):
         paths = self.files_table.get_selected_row_items(0)
         conditions = CheckModsContextMenuConditions(self.vcs, paths)
         GtkFilesContextMenu(self, data, self.base_dir, paths, 
-            conditions=conditions)
+            conditions=conditions).show()
 
 class CheckModsContextMenuConditions(GtkFilesContextMenuConditions):
     def __init__(self, vcs_client, paths=[]):

rabbitvcs/ui/commit.py
@@ -170,7 +170,7 @@ class Commit(InterfaceView, GtkContextMenuCaller):
 
     def show_files_table_popup_menu(self, treeview, data):
         paths = self.files_table.get_selected_row_items(1)
-        GtkFilesContextMenu(self, data, self.base_dir, paths)
+        GtkFilesContextMenu(self, data, self.base_dir, paths).show()
 
     def delete_items(self, widget, data=None):
         paths = self.files_table.get_selected_row_items(1)

rabbitvcs/ui/lock.py
@@ -131,7 +131,7 @@ class Lock(InterfaceView, GtkContextMenuCaller):
 
     def show_files_table_popup_menu(self, treeview, data):
         paths = self.files_table.get_selected_row_items(1)
-        GtkFilesContextMenu(self, data, self.base_dir, paths)
+        GtkFilesContextMenu(self, data, self.base_dir, paths).show()
             
     #
     # UI Signal Callbacks

commit 45e75c8a6765498c5155486fff65b10e680177bc

Fixes a regression introduced in r2005 when no paths are passed to the context menu class
git-svn-id: http://rabbitvcs.googlecode.com/svn/trunk@2006 a8b49908-ab93-11de-b6f4-e1be98fab775

rabbitvcs/lib/contextmenu.py
@@ -521,9 +521,6 @@ class GtkFilesContextMenu:
     def __init__(self, caller, event, base_dir, paths=[], 
             conditions=None, callbacks=None):
         
-        if len(paths) == 0:
-            return
-        
         self.caller = caller
         self.event = event
         self.paths = paths
@@ -737,5 +734,8 @@ class GtkFilesContextMenu:
         ]
     
     def show(self):
+        if len(self.paths) == 0:
+            return
+
         context_menu = GtkContextMenu(self.items)
         context_menu.show(self.event)