35c6649b90cd08a8501e40705be68c3e02b4be0d
Daniel O'Connor <daniel.oconnor@gmail.com>
2012-09-08 12:42:03 +0930

    Issue 496:	Keyboard shortcut for closing dialogues Shortcuts added to 'checkmods' ui

--- rabbitvcs/ui/checkmods.py
+++ rabbitvcs/ui/checkmods.py
@@ -91,7 +91,21 @@ class SVNCheckForModifications(InterfaceView):
         if page_num == 1 and self.remote_refreshed == False:
             self.remote_mods.refresh()
             self.remote_refreshed = True
+            
+    def on_key_pressed(self, widget, data):
+        if (data.keyval == gtk.keysyms.Escape):
+            self.on_close_clicked(widget)
+            return True
+            
+        if (data.state & gtk.gdk.CONTROL_MASK and 
+                gtk.gdk.keyval_name(data.keyval).lower() == "w"):
+            self.on_close_clicked(widget)
+            return True
 
+        if (data.state & gtk.gdk.CONTROL_MASK and 
+                gtk.gdk.keyval_name(data.keyval).lower() == "q"):
+            self.on_close_clicked(widget)
+            return True
     #
     # Helper methods
     #
